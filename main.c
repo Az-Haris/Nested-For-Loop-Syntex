e8dabd7f6177edd4f5dc22538bc79d87e18c527e
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-02-02 21:25:06 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+
+    for(initialization; condition; increment/decrement)
+    {
+        for(initialization; condition; increment/decrement)
+        {
+            statement 1;
+            statement 2;
+            ...........
+            ...........
+            statement n;
+        }
+
+        statement 1;
+        statement 2;
+        statement 3;
+        ...........
+        ...........
+        statement n;
+    }
+
+
+    return 0;
+}
